2c2435a393711e53d178aba39605e5d399e1f80f
周岩 <zeroyan@zeroyan.local>
2015-07-22 09:56:10 +0800

    first building

---
+++ web/js/app.js
@@ -0,0 +1,39 @@
+/**
+ * Created by zeroyan on 15/7/19.
+ */
+
+
+var isCheckedBox = function(radio, box_children){
+    $("input[name="+radio+"]").change(function(){
+        flag = $(this).val();
+        //console.log(flag);
+        if(flag == 1){
+            for(var i = 0; i < box_children.length; i+=1){
+                var _this = $("input[name="+box_children[i]+"]")
+                var parent_div = _this.length==0 ? $("#"+box_children[i]).parent() : _this.parent().parent().parent().parent();
+                $(parent_div).fadeIn(500)
+            }
+        }else{
+            for(var i = 0; i < box_children.length; i+=1){
+                var _this = $("input[name="+box_children[i]+"]")
+                var parent_div = _this.length==0 ? $("#"+box_children[i]).parent() : _this.parent().parent().parent().parent();
+                $(parent_div).fadeOut(500)
+            }
+        }
+
+    });
+}
+
+var overlay = function(modal_id){
+    var e1 = document.getElementById(modal_id);
+    e1.style.visibility =  (e1.style.visibility == "visible"  ) ? "hidden" : "visible";
+}
+
+$(function(){
+    isCheckedBox('user_child_is18', ['user_child_num','user_child_gender']);
+    isCheckedBox('user_has_paycheck', ['user_paycheck_info']);
+    isCheckedBox('need_support_parents', ['support_parents_info','parents_insurance_info']);
+
+
+
+});
---
+++ web/js/evaluation.js
@@ -0,0 +1,28 @@
+/**
+ * Created by zeroyan on 15/7/21.
+ */
+
+var drawSector = function(id){
+    var canvas = document.getElementById(id);
+    if (canvas == null){
+        return false;
+    }
+    var context = canvas.getContext("2d");
+    color_list = ["rgb(251,71,64)","rgb(250，95，53)","rgb(252，122，43)","rgb(253，146，40)",
+                  "rgb(200,212,49)","rgb(180，222，49)","rgb(123，122，67)","rgb(51，216，119)",
+                  "rgb(36，217，139)","rgb(37，215，138)"];
+    start_angle = 0.75*Math.PI;
+    for(var i=0; i<10; i+=1){
+        context.beginPath();
+        context.arc(150,150,100,start_angle,start_angle+0.15*Math.PI,false);
+        context.lineWidth = 15;
+        context.strokeStyle = color_list[i];
+        context.stroke();
+        context.closePath();
+        start_angle += 0.15*Math.PI;
+    }
+};
+
+$(function(){
+    drawSector("sector_diagram");
+});
