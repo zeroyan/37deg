869d09363076d5e652a7deb9397cdf3eb3cfbb1d
周岩 <zeroyan@zeroyan.local>
2015-07-24 11:31:43 +0800

    mod:detait

--- web/js/app.js
+++ web/js/app.js
@@ -1,39 +1,126 @@
 /**
  * Created by zeroyan on 15/7/19.
  */
-
-
-var isCheckedBox = function(radio, box_children){
-    $("input[name="+radio+"]").change(function(){
-        flag = $(this).val();
-        //console.log(flag);
-        if(flag == 1){
-            for(var i = 0; i < box_children.length; i+=1){
-                var _this = $("input[name="+box_children[i]+"]")
-                var parent_div = _this.length==0 ? $("#"+box_children[i]).parent() : _this.parent().parent().parent().parent();
-                $(parent_div).fadeIn(500)
-            }
-        }else{
-            for(var i = 0; i < box_children.length; i+=1){
-                var _this = $("input[name="+box_children[i]+"]")
-                var parent_div = _this.length==0 ? $("#"+box_children[i]).parent() : _this.parent().parent().parent().parent();
-                $(parent_div).fadeOut(500)
-            }
-        }
-
-    });
-}
-
 var overlay = function(modal_id){
     var e1 = document.getElementById(modal_id);
     e1.style.visibility =  (e1.style.visibility == "visible"  ) ? "hidden" : "visible";
 };
 
+var formInteractive = {
+    isCheckedBox:function(radio, box_children){
+        $("input[name="+radio+"]").change(function(){
+            flag = $(this).val();
+            //console.log(flag);
+            if(flag == 1){
+                for(var i = 0; i < box_children.length; i+=1){
+                    var _this = $("input[name="+box_children[i]+"]")
+                    var parent_div = _this.length==0 ? $("#"+box_children[i]).parent() : _this.parent().parent().parent().parent();
+                    $(parent_div).fadeIn(500)
+                }
+            }else{
+                for(var i = 0; i < box_children.length; i+=1){
+                    var _this = $("input[name="+box_children[i]+"]")
+                    var parent_div = _this.length==0 ? $("#"+box_children[i]).parent() : _this.parent().parent().parent().parent();
+                    $(parent_div).fadeOut(500)
+                }
+            }
+
+        });
+    },
+    checkBoxNumber:function(clickId, changeId){
+        var _that = $("input[name="+clickId+"]");
+        $(_that).change(function(){
+            var find_active = $("input[name="+clickId+"]:checked");
+            //console.log(find_active.length);
+            if(find_active.length > 1){
+                var div = $("#"+changeId+"_div");
+                div.empty();
+                div.addClass("app-form-points").attr("id",changeId);
+                var add_span = $("<span>").text('…');
+                add_span.click(function(){overlay('user_child_gender_modal')});
+                div.append(add_span);
+            }else if(find_active.length == 1){
+                var div = $("#"+changeId);
+                div.empty();
+                div.removeClass("app-form-points").attr("id",changeId+"_div");
+                var label1 = $("<label>").addClass("am-btn am-btn-default").append(
+                    $("<input type='radio'>").attr("name",changeId)
+                ).text("男");
+                var label2 = $("<label>").addClass("am-btn am-btn-default").append(
+                    $("<input type='radio'>").attr("name",changeId)
+                ).text("女");
+                var label_div = $("<div id='app-form-radio' class='am-btn-group am-fr' data-am-button>").append(label1,label2);
+                div.append(label_div);
+            }
+        });
+    },
+    addMoreChild:function(){
+        var box = $($(this).parent()).find("#more_children");
+        var label1 = $("<label>").addClass("am-btn am-btn-default").append(
+            $("<input type='radio'>").attr("name","user_child_gender")
+        ).text("男");
+        var label2 = $("<label>").addClass("am-btn am-btn-default").append(
+            $("<input type='radio'>").attr("name","user_child_gender")
+        ).text("女");
+        var label_div = $("<div id='app-form-radio' class='am-btn-group am-fr' data-am-button>").append(label1,label2);
+        var div = $("<div class='am-g form-group'>").append(
+            $("<div class='am-u-sm-5'>").text("子女3"),
+            $("<div class='am-u-sm-7'>").append(label_div)
+        );
+        box.append(div);
+    },
+    toggleCheckBox:function(id){
+        var buttons = $("#"+id+" button");
+        //console.log(buttons);
+        for(var i=0; i<buttons.length; i+=1){
+            //console.log($(buttons[i]));
+            //console.log(buttons[i].value);
+            var b = $(buttons[i]);
+
+            if(buttons[i].value == ""){
+                b.click(function(){
+                    if($(this).hasClass("app-btn-active")){
+                        $(this).removeClass("app-btn-active");
+                    }else{
+                        $(this).addClass("app-btn-active");
+                    }
+                });
+            }else if(buttons[i].value == "0"){
+                b.click(function(ind){
+                    return function(){
+                        var b_toggle = $(buttons[ind]);
+                        var word = b_toggle.html();
+                        //console.log(word.slice(0,2));
+                        if(word.slice(0,2) == "收起"){
+                            b_toggle.html("更多<span class='am-icon-angle-double-up am-icon-sm'></span>");
+                            b_toggle.val("1");
+                            $(buttons[3]).css("display","inline");
+                            $(buttons[4]).css("display","inline");
+                            $(buttons[5]).css("display","inline");
+                        }else{
+                            b_toggle.html("收起<span class='am-icon-angle-double-down am-icon-sm'></span>");
+                            b_toggle.val("0");
+                            $(buttons[3]).css("display","none");
+                            $(buttons[4]).css("display","none");
+                            $(buttons[5]).css("display","none");
+                        }
+                    };
+                }(i));
+            }
+        }
+    }
+};
+
 $(function(){
-    isCheckedBox('user_child_is18', ['user_child_num','user_child_gender']);
-    isCheckedBox('user_has_paycheck', ['user_paycheck_info']);
-    isCheckedBox('need_support_parents', ['support_parents_info','parents_insurance_info']);
+    formInteractive.isCheckedBox('user_child_is18', ['user_child_num','user_child_gender']);
+    formInteractive.isCheckedBox('user_has_paycheck', ['user_paycheck_info']);
+    formInteractive.isCheckedBox('need_support_parents', ['support_parents_info','parents_insurance_info']);
+    formInteractive.checkBoxNumber('user_child_num','user_child_gender');
+    formInteractive.toggleCheckBox('app-form-checkbox');
+    $(".app-add-child").click(formInteractive.addMoreChild);
     $("#submit_form").click(function(){
         location.href = "evaluation.html";
     });
+    //console.log($(".modal-data>button"));
+
 });
--- web/js/evaluation.js
+++ web/js/evaluation.js
@@ -1,10 +1,26 @@
 /**
  * Created by zeroyan on 15/7/21.
  */
-
+function getObjSize(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
 var overlay = function(modal_id){
     var e1 = document.getElementById(modal_id);
     e1.style.visibility =  (e1.style.visibility == "visible"  ) ? "hidden" : "visible";
+};
+
+var drawImage = function(context, img, x, y, w, h){
+    context.drawImage(img,x,y,w,h);
+};
+var writeText = function(context, text, x, y, algin, font, color){
+    context.font = font;
+    context.fillStyle = color;
+    context.textAlign = algin;
+    context.fillText(text,x,y);
 };
 
 var drawSector = function(id){
@@ -19,45 +35,195 @@ var drawSector = function(id){
     outline = new Image;
     outline.src = "./images/sector_outline.png";
     outline.onload = function(){
-        drowImage(context, outline);
-    };
-    start_angle = 0.9*Math.PI;
-    for(var i=0; i<10; i+=1){
+        drawImage(context,outline,2,1,297,200);
+
+        start_angle = 0.9*Math.PI;
+        for(var i=0; i<10; i+=1){
+            context.beginPath();
+            context.arc(150,150,84,start_angle,start_angle+0.121*Math.PI,false);
+            context.lineWidth = 1;
+            context.strokeStyle = "#cccccc";
+            context.stroke();
+            context.closePath();
+            start_angle += 0.121*Math.PI;
+        }
         context.beginPath();
-        context.arc(150,150,90,start_angle,start_angle+0.121*Math.PI,false);
+        context.arc(150,150,105,0.9*Math.PI,2.11*Math.PI,false);
         context.lineWidth = 1;
         context.strokeStyle = "#cccccc";
         context.stroke();
         context.closePath();
-        start_angle += 0.121*Math.PI;
+        writeText(context,"BETA",canvas.width/2,canvas.height-120,"center","10px Calibri", "white");
+        writeText(context,"86",canvas.width/2,canvas.height-70,"center","40px Calibri","green");
+        writeText(context,"保障良好",canvas.width/2,canvas.height-50,"center","2px Calibri","green");
+        writeText(context,"评估时间:2015-07-07",canvas.width/2,canvas.height-30,"center","2px Calibri","gray");
+        start_angle = 0.9*Math.PI;
+        var draw_result_line = setInterval(function(){
+            context.beginPath();
+            context.arc(150,150,105,0.9*Math.PI,start_angle+0.01*Math.PI,false);//终点需要改变
+            context.lineWidth = 5;
+            context.strokeStyle = "rgb(40,196,149)";
+            context.stroke();
+            context.closePath();
+
+
+            var point_x = 150+105*Math.cos(start_angle);
+            var point_y = 150+105*Math.sin(start_angle);
+            context.clearRect(point_x-9,point_y-9,18,18);
+            context.beginPath();
+            context.arc(point_x,point_y,3,2*Math.PI,false);//终点需要改变
+            //context.arc(150,150,5,2*Math.PI,false);//终点需要改变
+            //context.lineWidth = 1;
+            context.strokeStyle = "rgb(40,196,149)";
+            context.stroke();
+            context.closePath();
+            context.fillStyle = "#EDEDED";
+            context.fill();
+
+            start_angle += 0.009*Math.PI;
+            if(start_angle > 1.78*Math.PI){
+                clearInterval(draw_result_line);
+            }
+        },10);
+    };
+};
+
+var drawSemiCircle = function(id, arr){
+    var canvas = document.getElementById(id);
+    if (canvas == null){
+        return false;
     }
+    var context = canvas.getContext("2d");
+    var width = canvas.width;
+    var height = canvas.height;
+    outline = new Image;
+    outline.src = "./images/semi_legend.jpg";
+    outline.onload = function(){
+        drawImage(context, outline, width-120, height/2 - 35, 100, 70);
+    };
+    writeText(context,"住院",35,20,"center ","1pt SimSun","black");
+    writeText(context,"门诊",90,20,"center ","1pt SimSun","black")
     context.beginPath();
-    context.arc(150,150,105,0.9*Math.PI,2.11*Math.PI,false);
+    context.arc(70,80,55,0.47*Math.PI,1.53*Math.PI,false);
+    context.closePath();
     context.lineWidth = 1;
     context.strokeStyle = "#cccccc";
     context.stroke();
+    context.beginPath();
+    context.arc(70,80,55,0.47*Math.PI,(0.47+arr[0])*1.04*Math.PI,false);
+    //var point_x = 70+55*Math.cos((0.47+arr[0])*1.04*Math.PI);
+    //var point_y = 80+55*Math.sin((0.47+arr[0])*1.04*Math.PI);
+    context.lineTo(75, 80+55*Math.sin((0.47+arr[0])*1.04*Math.PI));
+    //context.quadraticCurveTo(point_x + 10*arr[0], point_y - 10*arr[0], 70-point_x, point_y);
+    //context.quadraticCurveTo(70-point_x + 10*arr[0], point_y + 10*arr[0], 75, point_y);
+    //context.quadraticCurveTo();
     context.closePath();
-    start_angle = 0.9*Math.PI;
-    var draw_result_line = setInterval(function(){
-        context.beginPath();
-        context.arc(150,150,105,start_angle,start_angle+0.01*Math.PI,false);//终点需要改变
-        context.lineWidth = 5;
-        context.strokeStyle = "rgb(40,196,149)";
-        context.stroke();
-        context.closePath();
-        start_angle += 0.01*Math.PI;
-        if(start_angle > 1.5*Math.PI){
-            clearInterval(draw_result_line);
+    context.lineWidth = 1;
+    context.strokeStyle = "#cccccc";
+    context.fillStyle = "rgb(252,137,162)";
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(85,80,55,1.47*Math.PI,0.53*Math.PI,false);
+    context.closePath();
+    context.lineWidth = 1;
+    context.strokeStyle = "#cccccc";
+    context.fillStyle = "rgb(176,51,78)";
+    context.stroke();
+    context.fill();
+    context.beginPath();
+    context.arc(85,80,55,1.47*Math.PI,(1.47+arr[1])*1.04*Math.PI,false);
+    //context.arc(85,80,55,1.47*Math.PI,2*Math.PI,false);
+    //var point_x = 85+55*Math.cos((1.47+arr[1])*1.04*Math.PI);
+    //var point_y = 80+55*Math.sin((1.47+arr[1])*1.04*Math.PI);
+    //context.quadraticCurveTo(point_x - 10*arr[1], point_y + 10*arr[1], (85+point_x)/2, point_y);
+    //context.quadraticCurveTo((85+point_x)/2, point_y, 85, 80);
+    context.lineTo(80, 80+55*Math.sin((1.47+arr[1])*1.04*Math.PI));
+    context.closePath();
+    context.lineWidth = 1;
+    context.strokeStyle = "#cccccc";
+    context.fillStyle = "#ffffff";
+    context.stroke();
+    context.fill();
+};
+var drawColumn = function(id, dict){
+    var canvas = document.getElementById(id);
+    if (canvas == null){
+        return false;
+    }
+    var context = canvas.getContext("2d");
+    var width = canvas.width;
+    var height = canvas.height;
+    //console.log(dict);
+    var dict_len = getObjSize(dict);
+    //console.log(dict_len);
+    var sum = 0;
+    var i = 1;
+    for(var key in dict){
+        //console.log(key);
+        val = dict[key];
+        if(typeof(val) == "number"){
+            sum += val;
+            x = i * (width/dict_len)-40;
+            context.fillStyle = "rgb(253,137,161)";
+            context.fillRect(x-10,height-20,20,-(90*val/80));
+            context.beginPath();
+            context.moveTo(5, height-19.5);
+            context.lineTo(width-5, height-19.5);
+            context.closePath();
+            context.lineWidth = 1;
+            //context.strokeStyle = "rgb(253,137,161)";
+            context.strokeStyle = "#cccccc";
+            context.stroke();
+            writeText(context,key,x-25,height-5,"center ","10px sans-serif","black");
+            writeText(context,val.toString()+"万",x-13,height-(90*val/80)-25,"center ","10px SimSun","black");
+        }else if(typeof(val) == "object"){
+            //console.log(val[0])
+            sum = sum + val[0] + val[1];
+            x = i * (width/dict_len)-40;
+            context.fillStyle = "rgb(253,137,161)";
+            context.fillRect(x-15,height-20,15,-(90*val[0]/20));
+            writeText(context,val[0].toString()+"万",x-25,height-(90*val[0]/20)-25,"center ","10px SimSun","black");
+            context.fillStyle = "rgb(176,51,78)";
+            context.fillRect(x,height-20,15,-(90*val[1]/20));
+            writeText(context,val[1].toString()+"万",x,height-(90*val[1]/20)-25,"center ","10px SimSun","black");
+            context.beginPath();
+            context.moveTo(5, height-19.5);
+            context.lineTo(width-5, height-19.5);
+            context.closePath();
+            context.lineWidth = 1;
+            //context.strokeStyle = "rgb(253,137,161)";
+            context.strokeStyle = "#cccccc";
+            context.stroke();
+            writeText(context,key,x-25,height-5,"center ","10px sans-serif","black");
+            //writeText(context,val.toString()+"万",x-13,height-105,"center ","10px SimSun","black");
         }
-    },10);
-};
+        i += 1;
+    }
+    writeText(context,"一共需要"+sum+"万",5,15,"center ","10px SimSun","black");
+    //for(var i=1; i<5; i++){
+    //    console.log(dict[i]);
+    //    x = i * (width/4)-40;
+    //    context.fillStyle = "rgb(253,137,161)";
+    //    context.fillRect(x-10,height-20,20,-80);
+    //    context.beginPath();
+    //    context.moveTo(5, height-19.5);
+    //    context.lineTo(width-5, height-19.5);
+    //    context.closePath();
+    //    context.lineWidth = 1;
+    //    //context.strokeStyle = "rgb(253,137,161)";
+    //    context.strokeStyle = "#cccccc";
+    //    context.stroke();
+    //    writeText(context,"子女教育",x-25,height-5,"center ","10px sans-serif","black")
+    //    writeText(context,"10万",x-13,height-105,"center ","10px SimSun","black")
+    //}
 
-var drowImage = function(context, img){
-    context.drawImage(img,10,3,280,200);
 };
-
 $(function(){
     drawSector("sector_diagram");
+    drawSemiCircle("semicircle_diagram",[0.4,0.7]);
+    drawColumn("column_diagram", {"子女教育":[5,17],"子女抚养":49,"父母赡养":60,"房屋贷款":50});
 
     $("#consult").click(function(e){
         e.stopPropagation();
